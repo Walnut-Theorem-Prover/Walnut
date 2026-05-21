5edb4f411b5828adb5666f363c703f7c8962c73a
jn1z <jn1z@users.noreply.github.com>
2026-05-21 10:08:56 -0400

    Test Morphism image length.

--- src/test/java/Automata/MorphismTest.java
+++ src/test/java/Automata/MorphismTest.java
@@ -19,5 +19,14 @@ public class MorphismTest {
         Automaton P = h.toWordAutomaton();
         Assertions.assertEquals("[{0=>[0], 1=>[1]}, {0=>[2], 1=>[1]}, {0=>[0], 1=>[3]}, {0=>[2], 1=>[3]}]",
             P.getFa().getT().getNfaD().toString());
+        Assertions.assertEquals(2, h.length);
+    }
+
+    @Test
+    void testImageLength() {
+        Morphism h = new Morphism("0->01 1->21 2->03 3->23");
+        Assertions.assertEquals(2, h.length);
+        h = new Morphism("0->0123 1->21 2->03 3->23");
+        Assertions.assertEquals(-1, h.length);
     }
 }
