0ea5bb5e1b364d17b3c5556afc27285ae14806be
Markus Frohme <markus.frohme@udo.edu>
2026-05-26 16:53:39 +0200

    initial refactoring

--- build.sh
+++ build.sh
@@ -7,11 +7,11 @@ SCRIPTNAME="walnut.sh"
 if [[ $1 = "-t" ]]; then
 	# run with tests
 	echo "Building Walnut and running tests."
-	./gradlew clean build test jacocoTestReport customFatJar 
+	./mvnw clean package -Pcode-coverage,fat-jar
 else
 	# If you want a fast build without tests, you can run:
 	echo "Building Walnut. To run tests, add the -t flag to the command."
-	./gradlew clean customFatJar
+	./mvnw clean package -DskipTests -Pfat-jar
 fi	
 
 chmod +x $SCRIPTNAME
--- walnut.sh
+++ walnut.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-java -jar build/libs/Walnut-all.jar "$@"
+java -jar target/Walnut-all.jar "$@"
 
 #Invoked with 15GB of memory
 #java -Xmx15000M -jar build/libs/Walnut-all.jar "$@"
