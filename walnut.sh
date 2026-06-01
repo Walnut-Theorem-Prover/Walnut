2978107d93ac05eb677d9ba30af21cc109e49ca4
Markus Frohme <markus.frohme@udo.edu>
2026-06-01 21:36:02 +0200

    walnut.sh: suppress maven output

--- walnut.sh
+++ walnut.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Invoked with 15GB memory. Change as you see fit.
-./mvnw exec:java -Dexec.mainClass="Main.Prover" -Dexec.args="$*"
+./mvnw -q exec:java -Dexec.mainClass="Main.Prover" -Dexec.args="$*"
 
 #Invoked with 15GB of memory
 #MAVEN_OPTS="${MAVEN_OPTS} -Xmx15G" ./mvnw exec:java -Dexec.mainClass="Main.Prover" -Dexec.args="$*"
